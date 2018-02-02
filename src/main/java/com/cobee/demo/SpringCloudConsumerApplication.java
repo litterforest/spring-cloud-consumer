11888ac5fd78b4c2a87118bdc394851e192d844f
litterforest <18027041863@163.com>
2018-02-02 09:47:19 +0800

    使用maven注意包的导入，容易出现烂包

--- src/main/java/com/cobee/demo/SpringCloudConsumerApplication.java
+++ src/main/java/com/cobee/demo/SpringCloudConsumerApplication.java
@@ -2,9 +2,9 @@ package com.cobee.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
